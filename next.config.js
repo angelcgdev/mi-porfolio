040b97d483f061d86575cab8ada5ae7a39a7344a
Luis Angel Caro Galoc <77682546+angelcgdev@users.noreply.github.com>
2023-04-24 08:11:30 -0500

    backup

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'cdn.shopify.com'],
   },
+  eslint: true,
 }
 
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta name="theme-color" content="rgb(var(--color-primary))" />
+      </Head>
       <body>
         <Main/>
         <NextScript />
